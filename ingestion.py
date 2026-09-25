954507dabb94852cf752d46e9208439711805aa2
VishalSharma00123 <vishal2k21ml@gmail.com>
2026-09-25 21:27:24 +0530

    updtate

--- ingestion.py
+++ ingestion.py
@@ -5,10 +5,10 @@ from typing import Any, Dict, List
 
 import certifi
 from dotenv import load_dotenv
-from langchain_chroma import Chroma
+# from langchain_chroma import Chroma
 from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
-from langchain_openai import OpenAIEmbeddings
+from langchain_nvidia_ai_endpoints import NVIDIAEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from langchain_tavily import TavilyCrawl, TavilyExtract, TavilyMap
 
@@ -23,65 +23,65 @@ os.environ["SSL_CERT_FILE"] = certifi.where()
 os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
 
 
-embeddings = OpenAIEmbeddings(
-    model="text-embedding-3-small",
-    show_progress_bar=False,
+embeddings = NVIDIAEmbeddings(
+    model="nvidia/nemotron-3-embed-1b",
+    api_key=os.environ["NVIDIA_API_KEY"],
     chunk_size=50,
-    retry_min_seconds=10,
+    retry_min_second=10
 )
-vectorstore = Chroma(persist_directory="chroma_db", embedding_function=embeddings)
-# vectorstore = PineconeVectorStore(
-#     index_name="langchain-docs-2025", embedding=embeddings
-# )
+# vectorstore = Chroma(persist_directory="chroma_db", embedding_function=embeddings)
+vectorstore = PineconeVectorStore(
+    index_name="langchain-docs-2025", embedding=embeddings
+)
 tavily_extract = TavilyExtract()
 tavily_map = TavilyMap(max_depth=5, max_breadth=20, max_pages=1000)
 tavily_crawl = TavilyCrawl()
 
 
-async def index_documents_async(documents: List[Document], batch_size: int = 50):
-    """Process documents in batches asynchronously."""
-    log_header("VECTOR STORAGE PHASE")
-    log_info(
-        f"📚 VectorStore Indexing: Preparing to add {len(documents)} documents to vector store",
-        Colors.DARKCYAN,
-    )
+# async def index_documents_async(documents: List[Document], batch_size: int = 50):
+#     """Process documents in batches asynchronously."""
+#     log_header("VECTOR STORAGE PHASE")
+#     log_info(
+#         f"📚 VectorStore Indexing: Preparing to add {len(documents)} documents to vector store",
+#         Colors.DARKCYAN,
+#     )
 
-    # Create batches
-    batches = [
-        documents[i : i + batch_size] for i in range(0, len(documents), batch_size)
-    ]
+#     # Create batches
+#     batches = [
+#         documents[i : i + batch_size] for i in range(0, len(documents), batch_size)
+#     ]
 
-    log_info(
-        f"📦 VectorStore Indexing: Split into {len(batches)} batches of {batch_size} documents each"
-    )
+#     log_info(
+#         f"📦 VectorStore Indexing: Split into {len(batches)} batches of {batch_size} documents each"
+#     )
 
-    # Process all batches concurrently
-    async def add_batch(batch: List[Document], batch_num: int):
-        try:
-            await vectorstore.aadd_documents(batch)
-            log_success(
-                f"VectorStore Indexing: Successfully added batch {batch_num}/{len(batches)} ({len(batch)} documents)"
-            )
-        except Exception as e:
-            log_error(f"VectorStore Indexing: Failed to add batch {batch_num} - {e}")
-            return False
-        return True
+#     # Process all batches concurrently
+#     async def add_batch(batch: List[Document], batch_num: int):
+#         try:
+#             await vectorstore.aadd_documents(batch)
+#             log_success(
+#                 f"VectorStore Indexing: Successfully added batch {batch_num}/{len(batches)} ({len(batch)} documents)"
+#             )
+#         except Exception as e:
+#             log_error(f"VectorStore Indexing: Failed to add batch {batch_num} - {e}")
+#             return False
+#         return True
 
-    # Process batches concurrently
-    tasks = [add_batch(batch, i + 1) for i, batch in enumerate(batches)]
-    results = await asyncio.gather(*tasks, return_exceptions=True)
+#     # Process batches concurrently
+#     tasks = [add_batch(batch, i + 1) for i, batch in enumerate(batches)]
+#     results = await asyncio.gather(*tasks, return_exceptions=True)
 
-    # Count successful batches
-    successful = sum(1 for result in results if result is True)
+#     # Count successful batches
+#     successful = sum(1 for result in results if result is True)
 
-    if successful == len(batches):
-        log_success(
-            f"VectorStore Indexing: All batches processed successfully! ({successful}/{len(batches)})"
-        )
-    else:
-        log_warning(
-            f"VectorStore Indexing: Processed {successful}/{len(batches)} batches successfully"
-        )
+#     if successful == len(batches):
+#         log_success(
+#             f"VectorStore Indexing: All batches processed successfully! ({successful}/{len(batches)})"
+#         )
+#     else:
+#         log_warning(
+#             f"VectorStore Indexing: Processed {successful}/{len(batches)} batches successfully"
+#         )
 
 
 async def main():
